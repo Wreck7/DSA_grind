712cbd27c7f056ef4a6bbbc3f0782d07f42497ea
Vishwa <vishwagovula07@gmail.com>
2025-09-25 21:25:09 +0530

    done with dsa day1

---
+++ dsa1.js
@@ -0,0 +1,70 @@
+const prompt = require("prompt-sync")();
+
+// let a = Number(prompt('enter a number: '))
+// let b = Number(prompt('enter a number: '))
+
+// console.log(a+b)
+
+// let a = Number(prompt('enter a number: '))
+// let b = Number(prompt('enter a number: '))
+
+// console.log(`sum of the numbers ${a+b}`)
+
+// swap two numbers
+
+// let a = 70, b = 80
+
+// let temp = a
+// a = b
+// b = temp
+
+// console.log(a,b)
+
+// let a = 70, b = 80;
+
+// [a, b] = [b, a]
+
+// console.log(a,b)
+
+// let a = 70, b = 80;
+
+// a = a + b;
+// b = a - b;
+// a = a - b;
+// console.log(a,b)
+
+// area and perimeter of rectangle
+
+// let l = Number(prompt('enter a length of the rectangle: '))
+// let w = Number(prompt('enter a width of the rectangle: '))
+
+// let area = l * w
+// let perimeter = 2 * (l+w)
+// console.log(`area of the rectangle ${area}`)
+// console.log(`perimeter of the rectangle ${perimeter}`)
+
+// area of triangle by herions formula
+
+// let a = Number(prompt("enter side a of the triangle: "));
+// let b = Number(prompt("enter side b of the triangle: "));
+// let c = Number(prompt("enter side c of the triangle: "));
+
+// function calculateArea(a, b, c) {
+//   let s = (a + b + c) / 2;
+//   let area = Math.sqrt(s * (s - a) * (s - b) * (s - c));
+//   return area
+// }
+
+// console.log(calculateArea(a,b,c))
+
+
+// circumference of the circle
+
+// let r = Number(prompt("enter radius of the circle: "))
+
+// function calculateCircumference(r) {
+//     let circumference = 2*Math.PI*r
+//     return circumference
+// }
+
+// console.log(calculateCircumference(r))
