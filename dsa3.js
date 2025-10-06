527d90ae0ea3f37d0edcee454ee204b5088fe2bf
Vishwa <vishwagovula07@gmail.com>
2025-10-06 21:16:11 +0530

    done some loops!

---
+++ dsa3.js
@@ -0,0 +1,53 @@
+const prompt = require("prompt-sync")();
+
+// print n hellos
+
+// function printHellos(n) {
+//     for (let i = 1; i <= n; i++) {
+//         console.log('hello!')
+//     }
+// }
+
+// let n = Number(prompt('enter a number: '))
+// printHellos(n)
+
+// one to n and vice versa
+
+// function oneToNAndBack(n) {
+//   for (let i = 1; i <= n; i++) {
+//     console.log(i);
+//   }
+//   for (let i = n-1; i >= 1; i--) {
+//     console.log(i);
+//   }
+// }
+
+// let n = Number(prompt("enter a number: "));
+// oneToNAndBack(n);
+
+
+// sum of natural numbers
+
+// function sumOfNaturalNums(n){
+//     let sum = 0;
+//     for(let i = 1; i <=n; i++){
+//         sum += i
+//     }
+//     return sum
+// }
+
+// let n = Number(prompt("enter a number: "));
+// console.log(sumOfNaturalNums(n))
+
+// factorial of a n numbers
+
+// function factorial(n){
+//     let ans = 1
+//     for (let i = 1; i <= n; i++) {
+//         ans *= i
+//     }
+//     return ans
+// }
+
+// let n = Number(prompt("enter a number: "));
+// console.log(factorial(n))
