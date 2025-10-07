da67dc26e37dffca3527b13754891941ca23bd0c
Vishwa <vishwagovula07@gmail.com>
2025-10-07 20:42:31 +0530

    done some pattern problems!

---
+++ dsa4.js
@@ -0,0 +1,34 @@
+// triangle pattern printing
+
+// let n = 7;
+
+// for (let i = 1; i <= n; i++) {
+//   console.log('* '.repeat(i))
+// }
+
+// reverse triangle pattern printing
+
+// let n = 7;
+
+// for (let i = 7; i >= 1; i--) {
+//     console.log("* ".repeat(i))
+// }
+
+
+// perfect triangle printing
+
+// let n = 7
+// for (let i = 1; i < n ; i++) {
+//     console.log(' '.repeat(n-i), '* '.repeat(i))
+// }
+
+
+// reverse perfect triangle printing
+
+// let n = 7
+
+// for (let i = n; i >= 1; i--){
+//    console.log(' '.repeat(n-i), '* '.repeat(i))
+// }
+
+
