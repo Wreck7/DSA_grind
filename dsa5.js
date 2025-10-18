8b6096b03d9d660718e2435c4fc6567a6685195b
Vishwa <vishwagovula07@gmail.com>
2025-10-18 19:58:55 +0530

    done some dsa

---
+++ dsa5.js
@@ -0,0 +1,31 @@
+const prompt = require("prompt-sync")();
+
+// taking items from user and added in arrays!
+
+// let arr = [];
+// function addItem() {
+//   let num = prompt("enter any number: ");
+//   while (num !== "") {
+//     arr.push(Number(num));
+//     num = prompt("enter any number: ");
+//   }
+// }
+
+// addItem();
+// console.log(arr);
+
+
+
+// sum of the numbers!
+
+// let arr = [1, 2, 3, 4, 5, 8];
+// let add = 0;
+// for (let i = 0; i < arr.length; i++) {
+//   add += arr[i];
+// }
+
+// console.log(add);
+
+
+
+// 
