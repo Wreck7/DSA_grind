e3867e8b0f8a0e73ab538e33226a9c26216ac683
Vishwa <vishwagovula07@gmail.com>
2025-11-12 22:51:40 +0530

    done!

--- dsa1.py
+++ dsa1.py
@@ -1,4 +1,13 @@
-a = int(input("enter a number: "))
-b = int(input("enter a number: "))
+# a = int(input("enter a number: "))
+# b = int(input("enter a number: "))
 
-print(f"the sum of a and b is {a+b}")
+# print(f"the sum of a and b is {a+b}")
+
+
+# swap two nums
+
+a = 1
+b = 2
+
+a, b = b, a
+print(a)
