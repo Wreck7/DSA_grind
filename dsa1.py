85d07b60b6fef03956fa17f888773ce457dd14af
Vishwa <vishwagovula07@gmail.com>
2025-11-17 21:29:53 +0530

    done some dsa prac py

--- dsa1.py
+++ dsa1.py
@@ -6,9 +6,21 @@
 
 # swap two nums
 
-a = 1
-b = 2
+# a = 1
+# b = 2
 
-a, b = b, a
-print(a)
-print(b)
+# a, b = b, a
+# print(a)
+# print(b)
+
+
+# area and perimeter
+
+l = int(input('enter a length of the rectangle: '))
+w = int(input('enter a width of the rectangle: '))
+
+area = l*w
+print(f"area of the rectangle {area}")
+
+perimeter = 2 * (l+w)
+print(f"perimeter of the rectangle {perimeter}")
