907b523f1cc25229d7886c7cec91806c69b265c4
Vishwa <vishwagovula07@gmail.com>
2025-11-18 22:49:51 +0530

    done!

--- dsa1.py
+++ dsa1.py
@@ -16,11 +16,25 @@
 
 # area and perimeter
 
-l = int(input('enter a length of the rectangle: '))
-w = int(input('enter a width of the rectangle: '))
+# l = int(input('enter a length of the rectangle: '))
+# w = int(input('enter a width of the rectangle: '))
 
-area = l*w
-print(f"area of the rectangle {area}")
+# area = l*w
+# print(f"area of the rectangle {area}")
 
-perimeter = 2 * (l+w)
-print(f"perimeter of the rectangle {perimeter}")
+# perimeter = 2 * (l+w)
+# print(f"perimeter of the rectangle {perimeter}")
+
+
+import math
+
+a = float(input("Enter side a of the triangle: "))
+b = float(input("Enter side b of the triangle: "))
+c = float(input("Enter side c of the triangle: "))
+
+def calculate_area(a, b, c):
+    s = (a + b + c) / 2
+    area = math.sqrt(s * (s - a) * (s - b) * (s - c))
+    return area
+
+print(calculate_area(a, b, c))
