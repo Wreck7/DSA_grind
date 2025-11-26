eba7abd6d1bf864920370be6b4adec16f52bccf5
Vishwa <vishwagovula07@gmail.com>
2025-11-26 23:07:24 +0530

    done!

--- dsa3.py
+++ dsa3.py
@@ -1,10 +1,22 @@
 # printing n hellos
 
 
+# n = int(input("enter a number: "))
+
+# def printing_n_hellos(n):
+#     for i in range(n):
+#         print("hello")
+        
+# printing_n_hellos(n)
+
+
+
 n = int(input("enter a number: "))
 
-def printing_n_hellos(n):
-    for i in range(n):
-        print("hello")
-        
-printing_n_hellos(n)
+def oneToNAndBack(n):
+    for i in range(1,n+1):
+        print(i)
+    for i in range(n,0,-1):
+        print(i)
+
+oneToNAndBack(n)
