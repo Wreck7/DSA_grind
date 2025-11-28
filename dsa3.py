daf70e94c2e3f59d1e96167a0136fadbdd1adae9
Vishwa <vishwagovula07@gmail.com>
2025-11-28 22:39:14 +0530

    done!

--- dsa3.py
+++ dsa3.py
@@ -11,12 +11,15 @@
 
 
 
-n = int(input("enter a number: "))
+# n = int(input("enter a number: "))
 
-def oneToNAndBack(n):
-    for i in range(1,n+1):
-        print(i)
-    for i in range(n,0,-1):
-        print(i)
+# def oneToNAndBack(n):
+#     for i in range(1,n+1):
+#         print(i)
+#     for i in range(n,0,-1):
+#         print(i)
 
-oneToNAndBack(n)
+# oneToNAndBack(n)
+
+
+
