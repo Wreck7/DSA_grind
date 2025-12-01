32af956c0e8d54d6530753a5c4e01fc01a93901f
Vishwa <vishwagovula07@gmail.com>
2025-12-01 18:53:25 +0530

    done some dsa python!

--- dsa3.py
+++ dsa3.py
@@ -23,3 +23,36 @@
 
 
 
+# def sum_of_natural_nums(n):
+#     total = 0
+#     for i in range(1, n + 1):
+#         total += i
+#     return total
+
+# n = int(input("Enter a number: "))
+# print(sum_of_natural_nums(n))
+
+
+
+
+# def factorial(n):
+#     ans = 1
+#     for i in range(1,n+1):
+#         ans *= i
+#     return ans
+
+# n = int(input("enter a number: "))
+# print(factorial(n))
+
+
+
+def getFactors(n):
+    factors = []
+    for i in range(1, n+1):
+        if n%i == 0:
+            factors.append(i)
+    return factors
+
+
+n = int(input("enter a number to get their factors: "))
+print(getFactors(n))
