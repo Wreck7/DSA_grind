1d1a4a3f711d838e81e8acc5e18ec33e469f6644
Vishwa <vishwagovula07@gmail.com>
2025-12-02 22:20:17 +0530

    done

--- dsa3.py
+++ dsa3.py
@@ -46,13 +46,36 @@
 
 
 
-def getFactors(n):
-    factors = []
-    for i in range(1, n+1):
-        if n%i == 0:
-            factors.append(i)
-    return factors
+# def getFactors(n):
+#     factors = []
+#     for i in range(1, n+1):
+#         if n%i == 0:
+#             factors.append(i)
+#     return factors
 
 
-n = int(input("enter a number to get their factors: "))
-print(getFactors(n))
+# n = int(input("enter a number to get their factors: "))
+# print(getFactors(n))
+
+
+
+#  prime number or not
+
+
+
+n = int(input("Enter a number: "))
+
+if n <= 1:
+    print("Not a prime number")
+else:
+    is_prime = True
+
+    for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+            is_prime = False
+            break
+
+    if is_prime:
+        print("Prime number")
+    else:
+        print("Not a prime number")
