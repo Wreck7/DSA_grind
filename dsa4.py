76a483cc946f94deb74b053083671a70cbfaa7fe
Vishwa <vishwagovula07@gmail.com>
2025-12-15 22:14:58 +0530

    done!

--- dsa4.py
+++ dsa4.py
@@ -12,6 +12,13 @@
 
 
 
+# n = 7
+# for i in range(1, n + 1):
+#     print(' ' * (n - i) + '* ' * i)
+    
+    
+
 n = 7
-for i in range(1, n + 1):
+
+for i in range(n, 0, -1):
     print(' ' * (n - i) + '* ' * i)
