3293a58821c7d329cf6891e8711102b6ea4e74a0
Vishwa <vishwagovula07@gmail.com>
2025-12-26 20:08:43 +0530

    done

--- ASE_interview.js
+++ ASE_interview.js
@@ -200,3 +200,28 @@
 // }
 
 // console.log(findSecondLargestConcise(numbersWithDuplicates));
+
+
+
+
+
+function moveZeros(arr) {
+  let index = 0;
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] !== 0) {
+      arr[index] = arr[i];
+      index++;
+    }
+  }
+
+  while (index < arr.length) {
+    arr[index] = 0;
+    index++;
+  }
+
+  return arr;
+}
+
+
+console.log(moveZeros([0, 1, 0, 3, 12]));
