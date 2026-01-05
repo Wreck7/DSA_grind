6cb99981220ab1bb6acb091ed83b575a19b9c712
Vishwa <vishwagovula07@gmail.com>
2026-01-05 21:40:43 +0530

    done

--- ASE_interview.js
+++ ASE_interview.js
@@ -266,13 +266,13 @@
 
 // Check if array is sorted
 
-function isSorted(arr) {
-  for (let i = 1; i < arr.length; i++) {
-    if (arr[i] < arr[i - 1]) {
-      return false;
-    }
-  }
-  return true;
-}
+// function isSorted(arr) {
+//   for (let i = 1; i < arr.length; i++) {
+//     if (arr[i] < arr[i - 1]) {
+//       return false;
+//     }
+//   }
+//   return true;
+// }
 
-console.log(isSorted([1, 2, 3, 4]));
+// console.log(isSorted([1, 2, 3, 4]));
